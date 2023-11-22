8160b25f87b3b6c38daaae07659935268f232595
Guilherme Jorge <guilherme.jorge010@gmail.com>
2023-11-22 13:50:48 -0300

    ref: excluded unused dependencies on index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application } from "express";
 import { connectToDatabase } from "./services/database.service";
 import { usersRouter } from "./routes/users.routes";
 import { messagesRouter } from "./routes/messages.routes";
